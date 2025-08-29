85f2c7e15d2cff70c89fa9766b5734363a311bcc
coffemakingtoaster <mh340@hdm-stuttgart.de>
2025-08-29 16:13:00 +0200

    update benchmark scripts

--- hado-benchmark/run.py
+++ hado-benchmark/run.py
@@ -1,11 +1,43 @@
-from os import listdir
-from os.path import isfile, join
+from os import listdir, makedirs
+from os.path import isfile, join, basename
 import subprocess
 import shlex
 import time
+from collections import defaultdict
+import json
+
+def parse_hado():
+    violation_results = defaultdict(lambda: 0)
+    all_files = get_filelist("./hado_out/")
+    for file in all_files:
+        with open(file) as f:
+            data = json.load(f)
+        for entry in data:
+            key = entry["code"]
+            violation_results[key] += 1
+
+    return violation_results
+
+def parse_ww():
+    violation_results = defaultdict(lambda: 0)
+    all_files = get_filelist("./ww_out/")
+    for file in all_files:
+        with open(file) as f:
+            lines = f.readlines()
+        for line in lines:
+            if not "ruleId=" in line:
+                continue
+            line = line.strip()
+            ind = line.find("ruleId=")
+            line = line[ind:]
+            line = line.lstrip("ruleId=")
+            rule_id = line.replace('"', "")
+            violation_results[rule_id] += 1
+
+    return violation_results
 
 def run(cmd, cwd=None, shell=False, executable="/bin/bash"):
-    print(f"\n>>> Running: {cmd}")
+    #print(f"\n>>> Running: {cmd}")
     if shell:
         return subprocess.run(cmd, shell=True, check=True, cwd=cwd, executable=executable)
     else:
@@ -15,30 +47,42 @@ def get_filelist(path: str):
     return [join(path, f) for f in listdir(path) if isfile(join(path, f))]
 
 def run_hadolint(filelist):
+    print(f"Running hado ({len(filelist)}) files so this may take a bit...")
+    out_dir = "./hado_out"
+
+    makedirs(out_dir, exist_ok=True)
+
     start = time.time()
-    for f in filelist:
-        try:
-            run(f'hadolint {f}', shell=True)
-        except KeyboardInterrupt:
-            return
-        except:
-            pass
+    for dockerfile in filelist:
+        outfile = join(out_dir, f"{basename(dockerfile)}.out")
+        run(f'hadolint -f json --no-fail {dockerfile}> {outfile}', shell=True)
     return time.time() - start
 
 def run_whale_watcher(filelist):
+    print(f"Running whale watcher ({len(filelist)}) files so this may take a bit...")
+
+    out_dir = "./ww_out"
+
+    makedirs(out_dir, exist_ok=True)
+
     start = time.time()
-    for f in filelist:
-        try:
-            run(f'WHALE_WATCHER_TARGET_DOCKERFILE="{f}" whale-watcher validate ./ruleset.yaml', shell=True)
-        except KeyboardInterrupt:
-            return
-        except:
-            pass
-    return time.time() -start
+
+    for dockerfile in filelist:
+        outfile = join(out_dir, f"{basename(dockerfile)}.out")
+        cmd = f'WHALE_WATCHER_TARGET_DOCKERFILE="{dockerfile}" whale-watcher validate ./ruleset.yaml > {outfile}'
+        run(cmd, shell=True)
+
+    return time.time() - start
 
 if __name__ == "__main__":
     files = get_filelist("./deduplicated-sources-gold/")
-    hado = run_hadolint(filelist=files)
-    ww = run_whale_watcher(filelist=files)
-    print(f"hado: {hado}")
-    print(f"ww: {ww}")
+    #hado = run_hadolint(filelist=files)
+    #ww = run_whale_watcher(filelist=files)
+    #print(f"hado: {hado}")
+    #print(f"ww: {ww}")
+    ww_results = parse_ww()
+    hado_results = parse_hado()
+
+    for key in set(list(hado_results.keys()) + list(ww_results.keys())):
+        print(f"ID:{key}\tTrivy: {hado_results[key]}\tWW: {ww_results[key]}")
+
--- trivy-benchmark/run.py
+++ trivy-benchmark/run.py
@@ -2,6 +2,46 @@ import subprocess
 import os
 import shlex
 import time
+from collections import defaultdict
+import json
+
+def parse_trivy():
+    violation_results = defaultdict(lambda: 0)
+    all_files = os.listdir("./trivy_out/")
+    for res in all_files:
+        filepath = os.path.join("./trivy_out/", res)
+        with open(filepath) as f:
+            data = json.load(f)
+        if not "Results" in data:
+            continue
+        results = data["Results"]
+        for entry in results:
+            if entry["MisconfSummary"]["Failures"] == 0:
+                continue
+            for violation in entry["Misconfigurations"]:
+                key = violation["Title"]
+                violation_results[key] += 1
+
+    return violation_results
+
+def parse_ww():
+    violation_results = defaultdict(lambda: 0)
+    all_files = os.listdir("./ww_out/")
+    for res in all_files:
+        filepath = os.path.join("./ww_out/", res)
+        with open(filepath) as f:
+            lines = f.readlines()
+        for line in lines:
+            if not "ruleId=" in line:
+                continue
+            line = line.strip()
+            ind = line.find("ruleId=")
+            line = line[ind:]
+            line = line.lstrip("ruleId=")
+            rule_id = line.replace('"', "")
+            violation_results[rule_id] += 1
+        
+    return violation_results
 
 def run_whale_watcher():
     src_dir = "./scraper/testdata"
@@ -24,6 +64,7 @@ def run_whale_watcher():
             outfile = os.path.join(out_dir, f"{subdir}.out")
             cmd = f'WHALE_WATCHER_TARGET_DOCKERFILE="{dockerfile}" WHALE_WATCHER_TARGET_DOCKER_PATH="{dockertar}" WHALE_WATCHER_TARGET_OCI_PATH="{ocitar}" whale-watcher validate ./ruleset.yaml > {outfile}'
             run(cmd, shell=True)
+
     return time.time() - start
 
 def run_trivy():
@@ -43,10 +84,11 @@ def run_trivy():
             if not all([os.path.exists(f) for f in [dockertar]]):
                 print(f"⚠️ Skipping {subdir_path}, needed files incomplete")
                 continue
-            outfile = os.path.join(out_dir, f"{subdir}.out")
-            cmd = f'trivy image --image-config-scanners misconfig --scanners misconfig -f json --input {dockertar}> {outfile}'
+            outfile_image = os.path.join(out_dir, f"{subdir}_image.out")
+            outfile_dockerfile = os.path.join(out_dir, f"{subdir}_dockerfile.out")
+            cmd = f'trivy image -q --image-config-scanners misconfig --scanners misconfig -f json --input {dockertar} -o {outfile_image}'
             run(cmd, shell=True)
-            cmd = f'trivy fs -f json --scanners misconfig {dockerfile} >> {outfile}'
+            cmd = f'trivy fs -f json --scanners misconfig {dockerfile} -o {outfile_dockerfile}'
             run(cmd, shell=True)
 
     return time.time() - start
@@ -73,6 +115,12 @@ def main():
 
     print(f"ww: {duration_ww}")
     print(f"trivy: {duration_trivy}")
+
+    trivy_res = parse_trivy()
+    ww_res = parse_ww()
+
+    for key in set(list(trivy_res.keys()) + list(ww_res.keys())):
+        print(f"ID:{key}\tTrivy: {trivy_res[key]}\tWW: {ww_res[key]}")
 
 
 if __name__ == "__main__":
