c7a76a84c345af83a7a92a4e324f39c57044f5de
coffemakingtoaster <mh340@hdm-stuttgart.de>
2025-09-13 20:40:31 +0200

    add convenience script

--- benchmark_all.sh
+++ benchmark_all.sh
@@ -1,2 +1,12 @@
-(cd ./hado-benchmark/ && python3 run.py > out1.log && python3 run.py > out2.log && python3 run.py > out3.log)
-(cd ./trivy-benchmark/ && python3 run.py > out1.log && python3 run.py > out2.log && python3 run.py > out3.log)
+run_benchmark(){
+	(cd $1 && rm -rf *_out )
+	(cd $1 && python3 run.py > out${2}.log)
+}
+
+run_benchmark ./hado-benchmark/ 1
+run_benchmark ./hado-benchmark/ 2
+run_benchmark ./hado-benchmark/ 3
+
+run_benchmark ./trivy-benchmark/ 1
+run_benchmark ./trivy-benchmark/ 2
+run_benchmark ./trivy-benchmark/ 3
